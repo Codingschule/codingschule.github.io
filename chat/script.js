45e4ebbb32f6f7023687b105e7259ea24d1003fe
Marc Bertram <marc.bertram@gmail.com>
2021-02-16 08:43:28 +0100

    update to githu

--- chat/script.js
+++ chat/script.js
@@ -28,7 +28,7 @@ const msgRef = db.ref("/msgs");
 var actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: 'http://localhost:5500/chat/chat.html',
+    url: 'http://training.codingschule.de/chat/chat.html',
     // This must be true.
     handleCodeInApp: true,
     /*iOS: {
